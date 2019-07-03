3b863b2413c28388287e37fb7c689033f0f20461
Payam Yasaie <payam.yasaie@outlook.com>
2019-07-03 13:27:40 +0430

    Service Provider added

---
+++ src/YasaieDictionaryProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Yasaie\Dicionary;
+
+use Illuminate\Support\ServiceProvider;
+
+class YasaieDictionaryProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
