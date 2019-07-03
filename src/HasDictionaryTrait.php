50575262248e5b60b098eb95c274b679e23d3194
Payam Yasaie <payam.yasaie@outlook.com>
2019-07-03 13:11:26 +0430

    First Initial

---
+++ src/HasDictionaryTrait.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package     shop
+ * @author      Payam Yasaie <payam@yasaie.ir>
+ * @copyright   2019-06-15
+ */
+
+namespace Yasaie\Dicionary;
+
+/**
+ * App\Product
+ *
+ * @mixin \Eloquent
+ */
+trait HasDictionaryTrait
+{
+    public function __get($key)
+    {
+        return $this->locale($key);
+    }
+
+    public function locale($key)
+    {
+        $dictionary = property_exists($this, 'dictionary')
+            ? $this->dictionary : [];
+
+        if (in_array($key, $dictionary)) {
+            return $this->getTranslate($key, \Config::get('app.locale'))
+                ?: $this->getTranslate($key, \Config::get('app.fallback_locale'));
+        }
+
+        return $this->getAttribute($key);
+    }
+
+    protected function getTranslate($name, $lang)
+    {
+        $req = [
+            $lang,
+            $this->table,
+            $this->id,
+            $name
+        ];
+        $key = implode('.', $req);
+
+        return \Cache::rememberForever($key, function() use ($req) {
+            return $this->dictionary()->where([
+                ['language_id', $req[0]],
+                ['key', $req[3]]
+            ])->value('value');
+        });
+    }
+
+    public function dictionary()
+    {
+        return $this->morphMany('App\\Dictionary', 'context');
+    }
+}
