1aa78d1eca0f171eb2d1b0dc2941a7a5cc674471
Payam Yasaie <payam.yasaie@outlook.com>
2019-07-21 15:42:30 +0430

    create locale

--- src/Dictionary.php
+++ src/Dictionary.php
@@ -17,13 +17,38 @@ class Dictionary extends Model
 
     protected $guarded = [];
 
+    /**
+     * @package context
+     * @author  Payam Yasaie <payam@yasaie.ir>
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
     public function context()
     {
         return $this->morphTo();
     }
 
+    /**
+     * @package getFullPathAttribute
+     * @author  Payam Yasaie <payam@yasaie.ir>
+     *
+     * @return string
+     */
     public function getFullPathAttribute()
     {
         return "{$this->context_type}{$this->context_id}";
+    }
+
+    public function createLocale($key, $values)
+    {
+        foreach ($values as $lang => $title) {
+            if ($title) {
+                $this->create([
+                    'key' => $key,
+                    'value' => $title,
+                    'language_id' => $lang
+                ]);
+            }
+        }
     }
 }
