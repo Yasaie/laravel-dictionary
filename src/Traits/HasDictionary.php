25652ceeac9c41bdfa99ed884a02b126cf9d1f43
Payam Yasaie <payam.yasaie@outlook.com>
2019-11-10 10:58:28 +0330

    added locales in doc and removed from property

--- src/Traits/HasDictionary.php
+++ src/Traits/HasDictionary.php
@@ -13,16 +13,11 @@ use Yasaie\Dictionary\Dictionary;
  * App\Product
  *
  * @mixin \Eloquent
+ *
+ * @property array $locales
  */
 trait HasDictionary
 {
-
-    /**
-     * @var array $locales
-     * names of available locales
-     */
-    protected $locales;
-
     /**
      * @author  Payam Yasaie <payam@yasaie.ir>
      * @since   2019-08-18
